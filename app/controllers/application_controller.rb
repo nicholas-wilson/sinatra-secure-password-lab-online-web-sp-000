10dc60a4900646845c11040022ed60241f4143e1
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-09 05:35:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,8 @@ class ApplicationController < Sinatra::Base
       user = User.find_by(username: params[:username])
       if !!user && validate(params[:password])
         redirect '/account'
+      else
+        redirect '/failure'
       end
     else
       redirect '/failure'
