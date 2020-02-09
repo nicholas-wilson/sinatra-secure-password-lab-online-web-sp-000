58f1add7f24b61ca7247e3aa59b679c69c998bfd
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-09 05:25:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     if params[:username] != "" && params[:password] != ""
+      binding.pry
       user = User.new(params)
       user.save
       redirect '/login'
